bbae1847521e8a09b761b6d010315724538e6712
thkmon <thkmon@gmail.com>
2021-01-24 02:03:07 +0900

    특정 포스트 업데이트 기능

--- BBNaverBlogParser/src/com/thkmon/blogparser/main/MainClass.java
+++ BBNaverBlogParser/src/com/thkmon/blogparser/main/MainClass.java
@@ -1,24 +1,18 @@
 package com.thkmon.blogparser.main;
 
 import java.net.URLDecoder;
-import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Hashtable;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import com.thkmon.blogparser.common.Const;
-import com.thkmon.blogparser.database.SimpleDBMapper;
-import com.thkmon.blogparser.database.SimpleDBUtil;
-import com.thkmon.blogparser.prototype.ObjList;
 import com.thkmon.blogparser.prototype.StringMap;
 import com.thkmon.blogparser.prototype.StringMapList;
 import com.thkmon.blogparser.util.HttpUtil;
 import com.thkmon.blogparser.util.JsonUtil;
+import com.thkmon.blogparser.wordpress.WordpressMapper;
 
 public class MainClass {
 
@@ -26,28 +20,35 @@ public class MainClass {
 	private static final String wordpressUrl = Const.optionProperties.get("wordpress_url");
 	private static final int beginPageNum = 1;
 	private static final int endPageNum = 1;
+	private static final int countPerPage = 5; // 5, 10, 15, 20, 30 가능
+	
 	
 	public static void main(String[] args) {
 		try {
 			MainClass mainCls = new MainClass();
-			mainCls.main();
+			
+			// 1. 최근 포스트 인서트 기능
+			// mainCls.insertRecentPosts();
+			
+			// 2. 특정 포스트 업데이트 기능
+			// mainCls.updatePost("221166770752");
+			
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	private void main() throws Exception {
-		// String folerPath = "C:\\test";
-		// String fileName = "naver_blog_article_numbers.txt";
-		// String filePath = folerPath + "\\" + fileName;
-		// File dirObj = new File(folerPath);
-		// if (!dirObj.exists()) {
-		// System.err.println("The folder does not exist. (" + folerPath + ")");
-		// return;
-		// }
-
-		// int beginPageNum = 1;
-		// int endPageNum = 1;
+	
+	
+	/**
+	 * 최근 포스트 인서트 기능
+	 * 
+	 * @throws SQLException
+	 * @throws Exception
+	 */
+	private void insertRecentPosts() throws SQLException, Exception {
 		StringMapList logNoList = new StringMapList();
 		for (int i = beginPageNum; i <= endPageNum; i++) {
 			logNoList = addNaverBlogArticleNumbers(logNoList, i);
@@ -57,23 +58,29 @@ public class MainClass {
 
 		System.out.println(logNoList);
 
+		WordpressMapper wordpressMapper = new WordpressMapper();
+		
 		int totalCount = logNoList.size();
 		for (int i = 0; i < totalCount; i++) {
 			StringMap map = logNoList.get(i);
-			insertWordpressPostFromNaverBlogPost(map);
+			wordpressMapper.insertWordpressPostFromNaverBlogPost(wordpressUrl, naverUserId, map);
 		}
-
-		// StringList fileContent = new StringList();
-		// int listCount = logNoList.size();
-		// if (listCount > 0) {
-		// for (int i = 0; i < listCount; i++) {
-		// fileContent.add(logNoList.get(i).get("logNo"));
-		// }
-		// }
-		//
-		// FileUtil.writeFile(filePath, fileContent, false);
 	}
-
+	
+	
+	/**
+	 * 특정 포스트 업데이트 기능
+	 * 
+	 * @param postNo
+	 * @throws SQLException
+	 * @throws Exception
+	 */
+	private void updatePost(String postNo) throws SQLException, Exception {
+		WordpressMapper wordpressMapper = new WordpressMapper();
+		wordpressMapper.updateWordpressPostFromNaverBlogPost(wordpressUrl, naverUserId, postNo);
+	}
+	
+	
 	/**
 	 * 특정 페이지의 포스트 정보들을 가져온다.
 	 * 
@@ -82,14 +89,14 @@ public class MainClass {
 	 * @return
 	 * @throws Exception
 	 */
-	private StringMapList addNaverBlogArticleNumbers(StringMapList logNoList, int pageNumber) throws Exception {
+	private StringMapList addNaverBlogArticleNumbers(StringMapList logNoList, int pageNumber) throws SQLException, Exception {
 		Hashtable<String, String> param = new Hashtable<String, String>();
 		param.put("blogId", naverUserId);
 		param.put("viewdate", "");
 		param.put("currentPage", String.valueOf(pageNumber));
 		param.put("categoryNo", "");
 		param.put("parentCategoryNo", "");
-		param.put("countPerPage", "30");
+		param.put("countPerPage", String.valueOf(countPerPage));
 
 		String strUrl = "https://blog.naver.com/PostTitleListAsync.nhn";
 		String result = HttpUtil.postHttp(strUrl, param, "UTF-8");
@@ -148,175 +155,5 @@ public class MainClass {
 		}
 
 		return logNoList;
-	}
-
-	/**
-	 * 네이버 블로그 포스트를 가져와서 워드프레스 DB 에 insert 한다.
-	 * 
-	 * @param targetMap
-	 * @throws Exception
-	 */
-	private void insertWordpressPostFromNaverBlogPost(StringMap targetMap) throws Exception {
-		if (targetMap == null) {
-			return;
-		}
-
-		String postNo = targetMap.get("logNo");
-		if (postNo == null || postNo.length() == 0) {
-			return;
-		}
-
-		Connection conn = null;
-		SimpleDBMapper mapper = new SimpleDBMapper();
-
-		try {
-			conn = SimpleDBUtil.getConnection();
-
-			{
-				String query = " SELECT COUNT(*) FROM wp_posts WHERE post_name = ? ";
-
-				ObjList objList = new ObjList();
-				objList.add(String.valueOf(postNo));
-
-				int rowCount = mapper.selectFirstInt(conn, query, objList, 0);
-				if (rowCount > 0) {
-					return;
-				}
-			}
-
-			// https://blog.naver.com/naverId/" + postNo
-			String urlString = "https://blog.naver.com/PostView.nhn?blogId=" + naverUserId + "&logNo=" + postNo + "&redirect=Dlog&widgetTypeCall=true&directAccess=false";
-
-			Document doc = Jsoup.connect(urlString).get();
-			Elements postViewAreas = doc.select("#postViewArea");
-			
-			// 스마트에디터 3.0
-			if (postViewAreas == null || postViewAreas.size() == 0) {
-				// postViewAreas = doc.select(".se_component_wrap");
-				System.err.println("해당 글은 가져올 수 없습니다. 스마트에디터 3.0인지 확인하시기 바랍니다.");
-				return;
-			}
-			
-			Element postViewArea = postViewAreas.get(0);
-			
-
-			String strHtml = postViewArea.html();
-			
-			int nextID = 0;
-			{
-				String query = " SELECT MAX(ID) + 1 FROM wp_posts ";
-				nextID = mapper.selectFirstInt(conn, query, null, 0);
-			}
-
-			{
-				StringBuffer buff = new StringBuffer();
-
-				buff.append(" INSERT INTO ");
-				buff.append(" wp_posts ( ");
-				buff.append(" 	ID, ");
-				buff.append(" 	post_author, ");
-				buff.append(" 	post_date, ");
-				buff.append(" 	post_date_gmt, ");
-				buff.append(" 	post_content, ");
-				buff.append(" 	post_title, ");
-				buff.append(" 	post_excerpt, ");
-				buff.append(" 	post_status, ");
-				buff.append(" 	comment_status, ");
-				buff.append(" 	ping_status, ");
-				buff.append(" 	post_password, ");
-				buff.append(" 	post_name, ");
-				buff.append(" 	to_ping, ");
-				buff.append(" 	pinged, ");
-				buff.append(" 	post_modified, ");
-				buff.append(" 	post_modified_gmt, ");
-				buff.append(" 	post_content_filtered, ");
-				buff.append(" 	post_parent, ");
-				buff.append(" 	guid, ");
-				buff.append(" 	menu_order, ");
-				buff.append(" 	post_type, ");
-				buff.append(" 	post_mime_type, ");
-				buff.append(" 	comment_count ");
-				buff.append(" ) values ( ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	?, ");
-				buff.append(" 	? ");
-				buff.append(" ) ");
-
-				String query = buff.toString();
-
-				String strTitle = targetMap.get("title");
-				String strDate = targetMap.get("addDate");
-				String[] strDateArr = strDate.split("\\.");
-				
-				try {
-					Integer.parseInt(strDateArr[0].trim());
-				} catch (NumberFormatException e) {
-					System.err.println("하루가 지나지 않은 포스트는 업로드할 수 없습니다.");
-					return;
-				}
-				
-				String strYear = String.format("%04d", Integer.parseInt(strDateArr[0].trim()));
-				String strMonth = String.format("%02d", Integer.parseInt(strDateArr[1].trim()));
-				String strDay = String.format("%02d", Integer.parseInt(strDateArr[2].trim()));
-
-				String strDate2 = strYear + "-" + strMonth + "-" + strDay + " " + "00:00:00";
-
-				ObjList objList = new ObjList();
-				objList.add(String.valueOf(nextID));
-				objList.add(String.valueOf("1"));
-				objList.add(String.valueOf(strDate2));
-				objList.add(String.valueOf(strDate2));
-				objList.add(String.valueOf(strHtml));
-				objList.add(String.valueOf(strTitle));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf("publish"));
-				objList.add(String.valueOf("open"));
-				objList.add(String.valueOf("open"));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf(postNo));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf(strDate2));
-				objList.add(String.valueOf(strDate2));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf("0"));
-				objList.add(String.valueOf(wordpressUrl + "/?p=" + postNo));
-				objList.add(String.valueOf("0"));
-				objList.add(String.valueOf("post"));
-				objList.add(String.valueOf(""));
-				objList.add(String.valueOf("0"));
-
-				boolean bInserted = mapper.insert(conn, query, objList);
-				System.out.println("bInserted ; " + bInserted);
-			}
-
-			SimpleDBUtil.commitAndClose(conn);
-
-		} catch (Exception e) {
-			throw e;
-
-		} finally {
-			SimpleDBUtil.rollbackAndClose(conn);
-		}
 	}
 }
---
+++ BBNaverBlogParser/src/com/thkmon/blogparser/wordpress/WordpressMapper.java
@@ -0,0 +1,398 @@
+package com.thkmon.blogparser.wordpress;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.thkmon.blogparser.database.SimpleDBMapper;
+import com.thkmon.blogparser.database.SimpleDBUtil;
+import com.thkmon.blogparser.prototype.ObjList;
+import com.thkmon.blogparser.prototype.StringMap;
+
+public class WordpressMapper {
+	
+	
+	/**
+	 * 네이버 블로그 포스트를 가져와서 워드프레스 DB 에 insert 한다.
+	 * 
+	 * @param wordpressUrl
+	 * @param naverUserId
+	 * @param targetMap
+	 * @throws SQLException
+	 * @throws Exception
+	 */
+	public void insertWordpressPostFromNaverBlogPost(String wordpressUrl, String naverUserId, StringMap targetMap) throws SQLException, Exception {
+		if (wordpressUrl == null || wordpressUrl.length() == 0) {
+			return;
+		}
+		
+		if (naverUserId == null || naverUserId.length() == 0) {
+			return;
+		}
+		
+		if (targetMap == null) {
+			return;
+		}
+
+		String postNo = targetMap.get("logNo");
+		if (postNo == null || postNo.length() == 0) {
+			return;
+		}
+
+		Connection conn = null;
+		SimpleDBMapper mapper = new SimpleDBMapper();
+
+		try {
+			conn = SimpleDBUtil.getConnection();
+
+			{
+				String query = " SELECT COUNT(*) FROM wp_posts WHERE post_name = ? ";
+
+				ObjList objList = new ObjList();
+				objList.add(String.valueOf(postNo));
+
+				int rowCount = mapper.selectFirstInt(conn, query, objList, 0);
+				if (rowCount > 0) {
+					return;
+				}
+			}
+			
+			
+			// https://blog.naver.com/naverId/" + postNo
+			String urlString = "https://blog.naver.com/PostView.nhn?blogId=" + naverUserId + "&logNo=" + postNo + "&redirect=Dlog&widgetTypeCall=true&directAccess=false";
+
+			Document doc = Jsoup.connect(urlString).get();
+			Elements postViewAreas = doc.select("#postViewArea");
+			
+			
+			// 스마트에디터 3.0 여부
+			boolean isSmartEditor3 = false;
+			if (postViewAreas == null || postViewAreas.size() == 0) {
+				isSmartEditor3 = true;
+			}
+			
+			
+			// 포스트 내용 가져오기
+			String strHtml = "";
+			if (!isSmartEditor3) {
+				Element postViewArea = postViewAreas.get(0);
+				strHtml = postViewArea.html();
+			} else {
+				postViewAreas = doc.select(".se_component_wrap");
+				Element postViewArea = postViewAreas.get(1);
+				strHtml = postViewArea.html();
+			}
+			
+			if (strHtml == null || strHtml.length() == 0) {
+				System.out.println("포스트 내용을 가져올 수 없습니다. (postNo : " + postNo + ")");
+				return;
+			}
+			
+			
+			int nextID = 0;
+			{
+				String query = " SELECT MAX(ID) + 1 FROM wp_posts ";
+				nextID = mapper.selectFirstInt(conn, query, null, 0);
+			}
+
+			{
+				StringBuffer buff = new StringBuffer();
+
+				buff.append(" INSERT INTO ");
+				buff.append(" wp_posts ( ");
+				buff.append(" 	ID, ");
+				buff.append(" 	post_author, ");
+				buff.append(" 	post_date, ");
+				buff.append(" 	post_date_gmt, ");
+				buff.append(" 	post_content, ");
+				buff.append(" 	post_title, ");
+				buff.append(" 	post_excerpt, ");
+				buff.append(" 	post_status, ");
+				buff.append(" 	comment_status, ");
+				buff.append(" 	ping_status, ");
+				buff.append(" 	post_password, ");
+				buff.append(" 	post_name, ");
+				buff.append(" 	to_ping, ");
+				buff.append(" 	pinged, ");
+				buff.append(" 	post_modified, ");
+				buff.append(" 	post_modified_gmt, ");
+				buff.append(" 	post_content_filtered, ");
+				buff.append(" 	post_parent, ");
+				buff.append(" 	guid, ");
+				buff.append(" 	menu_order, ");
+				buff.append(" 	post_type, ");
+				buff.append(" 	post_mime_type, ");
+				buff.append(" 	comment_count ");
+				buff.append(" ) values ( ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	?, ");
+				buff.append(" 	? ");
+				buff.append(" ) ");
+
+				String query = buff.toString();
+
+				
+				String strTitle = targetMap.get("title");
+				String strDate = targetMap.get("addDate");
+				String[] strDateArr = strDate.split("\\.");
+				
+				try {
+					Integer.parseInt(strDateArr[0].trim());
+				} catch (NumberFormatException e) {
+					System.err.println("하루가 지나지 않은 포스트는 업로드할 수 없습니다.");
+					return;
+				}
+				
+				String strYear = String.format("%04d", Integer.parseInt(strDateArr[0].trim()));
+				String strMonth = String.format("%02d", Integer.parseInt(strDateArr[1].trim()));
+				String strDay = String.format("%02d", Integer.parseInt(strDateArr[2].trim()));
+
+				String strDate2 = strYear + "-" + strMonth + "-" + strDay + " " + "00:00:00";
+
+				
+				ObjList objList = new ObjList();
+				objList.add(String.valueOf(nextID));
+				objList.add(String.valueOf("1"));
+				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(strHtml));
+				objList.add(String.valueOf(strTitle));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("publish"));
+				objList.add(String.valueOf("open"));
+				objList.add(String.valueOf("open"));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf(postNo));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("0"));
+				objList.add(String.valueOf(wordpressUrl + "/?p=" + postNo));
+				objList.add(String.valueOf("0"));
+				objList.add(String.valueOf("post"));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("0"));
+
+				boolean isInserted = mapper.insert(conn, query, objList);
+				System.out.println("isInserted ; " + isInserted);
+			}
+
+			SimpleDBUtil.commitAndClose(conn);
+
+		} catch (SQLException e) {
+			throw e;
+			
+		} catch (Exception e) {
+			throw e;
+
+		} finally {
+			SimpleDBUtil.rollbackAndClose(conn);
+		}
+	}
+	
+	
+	/**
+	 * 네이버 블로그 포스트를 가져와서 워드프레스 DB 에 insert 한다.
+	 * 
+	 * @param wordpressUrl
+	 * @param naverUserId
+	 * @param postNo
+	 * @throws SQLException
+	 * @throws Exception
+	 */
+	public void updateWordpressPostFromNaverBlogPost(String wordpressUrl, String naverUserId, String postNo) throws SQLException, Exception {
+		if (wordpressUrl == null || wordpressUrl.length() == 0) {
+			return;
+		}
+		
+		if (naverUserId == null || naverUserId.length() == 0) {
+			return;
+		}
+		
+		if (postNo == null || postNo.length() == 0) {
+			return;
+		}
+		
+		/*
+		if (targetMap == null) {
+			return;
+		}
+
+		String postNo = targetMap.get("logNo");
+		if (postNo == null || postNo.length() == 0) {
+			return;
+		}
+		*/
+		
+		Connection conn = null;
+		SimpleDBMapper mapper = new SimpleDBMapper();
+		
+		try {
+			conn = SimpleDBUtil.getConnection();
+			
+			String postID  = "";
+			{
+				String query = " SELECT ID FROM wp_posts WHERE post_name = ? ";
+
+				ObjList objList = new ObjList();
+				objList.add(String.valueOf(postNo));
+
+				postID = mapper.selectFirstString(conn, query, objList, "");
+				if (postID == null || postID.length() == 0) {
+					System.out.println("업데이트할 로우를 찾을 수 없습니다. (postNo : " + postNo + ")");
+					return;
+				}
+			}
+
+			// https://blog.naver.com/naverId/" + postNo
+			String urlString = "https://blog.naver.com/PostView.nhn?blogId=" + naverUserId + "&logNo=" + postNo + "&redirect=Dlog&widgetTypeCall=true&directAccess=false";
+
+			Document doc = Jsoup.connect(urlString).get();
+			Elements postViewAreas = doc.select("#postViewArea");
+			
+			
+			// 스마트에디터 3.0 여부
+			boolean isSmartEditor3 = false;
+			if (postViewAreas == null || postViewAreas.size() == 0) {
+				isSmartEditor3 = true;
+			}
+			
+			
+			// 포스트 내용 가져오기
+			String strHtml = "";
+			if (!isSmartEditor3) {
+				Element postViewArea = postViewAreas.get(0);
+				strHtml = postViewArea.html();
+			} else {
+				postViewAreas = doc.select(".se_component_wrap");
+				Element postViewArea = postViewAreas.get(1);
+				strHtml = postViewArea.html();
+			}
+			
+			if (strHtml == null || strHtml.length() == 0) {
+				System.out.println("포스트 내용을 가져올 수 없습니다. (postNo : " + postNo + ")");
+				return;
+			}
+			
+			
+			// 포스트 제목 가져오기
+			String strTitle = "";
+			try {
+				if (!isSmartEditor3) {
+					strTitle = doc.select(".itemSubjectBoldfont").get(0).html().trim();
+				}
+			} catch (NullPointerException e) {
+				// 제목을 못가져오는 경우 제목만 제외하고 나머지 업데이트 진행
+				strTitle = "";
+			} catch (Exception e) {
+				// 제목을 못가져오는 경우 제목만 제외하고 나머지 업데이트 진행
+				strTitle = "";
+			}
+			
+			
+			{
+				StringBuffer buff = new StringBuffer();
+
+				buff.append(" UPDATE wp_posts ");
+				buff.append(" SET ");
+				buff.append(" 	post_author = ?, ");
+//				buff.append(" 	post_date = ?, ");
+//				buff.append(" 	post_date_gmt = ?, ");
+				buff.append(" 	post_content = ?, ");
+				if (strTitle != null && strTitle.length() > 0) {
+					buff.append(" 	post_title = ?, ");
+				}
+				buff.append(" 	post_excerpt = ?, ");
+				buff.append(" 	post_status = ?, ");
+				buff.append(" 	comment_status = ?, ");
+				buff.append(" 	ping_status = ?, ");
+				buff.append(" 	post_password = ?, ");
+//				buff.append(" 	post_name = ?, ");
+				buff.append(" 	to_ping = ?, ");
+				buff.append(" 	pinged = ?, ");
+//				buff.append(" 	post_modified = ?, ");
+//				buff.append(" 	post_modified_gmt = ?, ");
+				buff.append(" 	post_content_filtered = ?, ");
+				buff.append(" 	post_parent = ?, ");
+				buff.append(" 	guid = ?, ");
+				buff.append(" 	menu_order = ?, ");
+				buff.append(" 	post_type = ?, ");
+				buff.append(" 	post_mime_type = ?, ");
+				buff.append(" 	comment_count = ? ");
+				buff.append(" WHERE ID = ? AND post_name = ? ");
+
+				String query = buff.toString();
+				
+				
+				ObjList objList = new ObjList();
+				objList.add(String.valueOf("1"));
+//				objList.add(String.valueOf(strDate2));
+//				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(strHtml));
+				if (strTitle != null && strTitle.length() > 0) {
+					objList.add(String.valueOf(strTitle));
+				}
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("publish"));
+				objList.add(String.valueOf("open"));
+				objList.add(String.valueOf("open"));
+				objList.add(String.valueOf(""));
+//				objList.add(String.valueOf(postNo));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf(""));
+//				objList.add(String.valueOf(strDate2));
+//				objList.add(String.valueOf(strDate2));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("0"));
+				objList.add(String.valueOf(wordpressUrl + "/?p=" + postNo));
+				objList.add(String.valueOf("0"));
+				objList.add(String.valueOf("post"));
+				objList.add(String.valueOf(""));
+				objList.add(String.valueOf("0"));
+				
+				objList.add(String.valueOf(postID));
+				objList.add(String.valueOf(postNo));
+
+				boolean isUpdated = mapper.insert(conn, query, objList);
+				System.out.println("isUpdated ; " + isUpdated);
+			}
+
+			SimpleDBUtil.commitAndClose(conn);
+
+		} catch (SQLException e) {
+			throw e;
+			
+		} catch (Exception e) {
+			throw e;
+
+		} finally {
+			SimpleDBUtil.rollbackAndClose(conn);
+		}
+	}
+}
